03bbfb33ea4610d390e90b4d8b4eb441edf79a91
SaharMirkamrani <shrmrkmrn@gmail.com>
2021-02-27 15:01:04 +0330

    Add fonts, add logo, add sass files

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo/logo.svg';
+// import styles from './styles/styles.module.scss'
+import styles from './styles/App.module.scss'
+import './styles/variables.module.scss';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+      <header>
+        {/* <img src={logo} alt="logo" /> */}
+        <p className={styles.test}>
+          این یک تست برای امتحان کردن فونت است.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
