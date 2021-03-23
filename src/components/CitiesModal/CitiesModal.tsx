77bd5a0c705ca5215d6bfa5d081774099758abdf
SaharMirkamrani <shrmrkmrn@gmail.com>
2021-03-23 10:35:43 +0430

    refactor cities modal

--- src/components/CitiesModal/CitiesModal.tsx
+++ src/components/CitiesModal/CitiesModal.tsx
@@ -1,48 +1,121 @@
 import React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { Button, Box } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { Link, useLocation } from 'react-router-dom';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
+import TextField from '@material-ui/core/TextField';
+import { preLoad } from '../../api/preLoadData';
+
+const topCities = [
+  ['تهران', 'tehran'],
+  ['مشهد', 'mashhad'],
+  ['کرج', 'karaj'],
+  ['شیراز', 'shiraz'],
+  ['اصفهان', 'isfahan'],
+  ['اهواز', 'ahvaz'],
+  ['تبریز', 'tabriz'],
+  ['کرمانشاه', 'kermanshah'],
+  ['قم', 'qom'],
+  ['رشت', 'rasht'],
+];
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    cityButton: {
+      color: '#555',
+      margin: '0px 5px',
+      backgroundColor: 'white',
+      fontFamily: 'Vazir',
+      padding: '7px 16px',
+      '&:hover': {
+        backgroundColor: '#hhh',
+        color: 'black',
+      },
+    },
     modal: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
+      display: 'block',
+      overflow: 'scroll',
+      position: 'absolute',
+      top: '10%',
+      left: '10%',
+      height: '100%',
+      marginTop: '30px',
+      border: 'none',
     },
     paper: {
-      backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
       boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
+      padding: theme.spacing(2, 1, 3),
+      backgroundColor: 'white',
+      width: '790px',
+      margin: 'auto',
+      border: 'none',
+      borderRadius: '10px',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: '25ch',
+    },
+    activeCityBtn: {
+      color: 'white',
+      margin: '12px',
+      backgroundColor: '#a62626',
+      padding: '5px',
+      fontSize: '16px',
+      fontFamily: 'Vazir',
+      width: '130px',
+      '&:hover': {
+        backgroundColor: '#be3737',
+      },
+    },
+    cityBtn: {
+      color: '#a62626',
+      margin: '12px',
+      backgroundColor: 'white',
+      textDecoration: 'none',
+      padding: '5px',
+      fontSize: '16px',
+      fontFamily: 'Vazir',
+      width: '130px',
+      borderColor: '#a62626',
+      '&:hover': {
+        backgroundColor: 'rgba(166,38,38,.04)',
+      },
+    },
+    resize: {
+      fontFamily: 'Vazir',
     },
   })
 );
 
-export default function TransitionsModal() {
+export default function CitiesModal() {
   const classes = useStyles();
+  const location = useLocation();
   const [open, setOpen] = React.useState(false);
 
-  const handleOpen = () => {
+  const handleModalOpen = () => {
     setOpen(true);
   };
 
-  const handleClose = () => {
+  const handleModalClose = () => {
     setOpen(false);
   };
-
   return (
-    <div>
-      <button type="button" onClick={handleOpen}>
-        react-transition-group
-      </button>
+    <>
+      <Link style={{ textDecoration: 'none' }} to="/tehran">
+        <Button className={classes.cityButton} onClick={handleModalOpen}>
+          <LocationOnIcon /> تهران
+        </Button>
+      </Link>
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         className={classes.modal}
         open={open}
-        onClose={handleClose}
+        onClose={handleModalClose}
         closeAfterTransition
         BackdropComponent={Backdrop}
         BackdropProps={{
@@ -51,13 +124,69 @@ export default function TransitionsModal() {
       >
         <Fade in={open}>
           <div className={classes.paper}>
-            <h2 id="transition-modal-title">Transition modal</h2>
-            <p id="transition-modal-description">
-              react-transition-group animates me.
+            <h3
+              style={{ fontFamily: 'Vazir', margin: '0px 5px 20px 5px' }}
+              id="transition-modal-title"
+            >
+              انتخاب شهر
+            </h3>
+            <TextField
+              id="outlined-full-width"
+              style={{ margin: 0 }}
+              placeholder="جستجوی سریع نام شهر..."
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              InputProps={{
+                classes: {
+                  input: classes.resize,
+                },
+              }}
+              variant="outlined"
+            />
+            <p
+              style={{ fontFamily: 'Vazir', margin: '30px 10px 10px' }}
+              id="transition-modal-description"
+            >
+              شهر های پر بازدید
             </p>
+            <Box>
+              {/* @ts-ignore */}
+              {topCities.map((city) => (
+                <Link style={{ textDecoration: 'none' }} to={`/${city[1]}`}>
+                  {location.pathname === `/${city[1]}` ? (
+                    <Button className={classes.activeCityBtn}>{city[0]}</Button>
+                  ) : (
+                    <Button variant="outlined" className={classes.cityBtn}>
+                      {city[0]}
+                    </Button>
+                  )}
+                </Link>
+              ))}
+            </Box>
+            <p
+              style={{ fontFamily: 'Vazir', margin: '30px 10px 10px' }}
+              id="transition-modal-description"
+            >
+              همه شهر ها
+            </p>
+            {/* @ts-ignore */}
+            {preLoad.city.compressedData.map((city) => (
+              <Link style={{ textDecoration: 'none' }} to={`/${city[2]}`}>
+                {location.pathname === `/${city[2]}` ? (
+                  <Button className={classes.activeCityBtn}>{city[1]}</Button>
+                ) : (
+                  <Button variant="outlined" className={classes.cityBtn}>
+                    {city[1]}
+                  </Button>
+                )}
+              </Link>
+            ))}
           </div>
         </Fade>
       </Modal>
-    </div>
+    </>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,28 +11,10 @@ import {
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuIcon from '@material-ui/icons/Menu';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
 import Logo from '../../logo/logo.svg';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from '../../styles/App.module.scss';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
-import TextField from '@material-ui/core/TextField';
-import { preLoad } from '../../api/preLoadData';
-
-const topCities = [
-  ['تهران', 'tehran'],
-  ['مشهد', 'mashhad'],
-  ['کرج', 'karaj'],
-  ['شیراز', 'shiraz'],
-  ['اصفهان', 'isfahan'],
-  ['اهواز', 'ahvaz'],
-  ['تبریز', 'tabriz'],
-  ['کرمانشاه', 'kermanshah'],
-  ['قم', 'qom'],
-  ['رشت', 'rasht'],
-];
+import CitiesModal from '../CitiesModal/CitiesModal';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -40,17 +22,6 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
       position: 'absolute',
       top: '0px',
-    },
-    cityButton: {
-      color: '#555',
-      margin: '0px 5px',
-      backgroundColor: 'white',
-      fontFamily: 'Vazir',
-      padding: '7px 16px',
-      '&:hover': {
-        backgroundColor: '#hhh',
-        color: 'black',
-      },
     },
     appbar: {
       direction: 'rtl',
@@ -110,68 +81,13 @@ const useStyles = makeStyles((theme: Theme) =>
     menuDropdown: {
       fontFamily: 'Vazir',
     },
-    modal: {
-      display: 'block',
-      overflow: 'scroll',
-      position: 'absolute',
-      top: '10%',
-      left: '10%',
-      height: '100%',
-      marginTop: '30px',
-      border: 'none',
-    },
-    paper: {
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 1, 3),
-      backgroundColor: 'white',
-      width: '790px',
-      margin: 'auto',
-      border: 'none',
-      borderRadius: '10px',
-    },
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: '25ch',
-    },
-    activeCityBtn: {
-      color: 'white',
-      margin: '12px',
-      backgroundColor: '#a62626',
-      padding: '5px',
-      fontSize: '16px',
-      fontFamily: 'Vazir',
-      width: '130px',
-      '&:hover': {
-        backgroundColor: '#be3737',
-      },
-    },
-    cityBtn: {
-      color: '#a62626',
-      margin: '12px',
-      backgroundColor: 'white',
-      textDecoration: 'none',
-      padding: '5px',
-      fontSize: '16px',
-      fontFamily: 'Vazir',
-      width: '130px',
-      borderColor: '#a62626',
-      '&:hover': {
-        backgroundColor: 'rgba(166,38,38,.04)',
-      },
-    },
-    resize: {
-      fontFamily: 'Vazir',
-    },
   })
 );
 
 export default function Navbar() {
   const classes = useStyles();
-  const location = useLocation();
   const desktop = useMediaQuery('(min-width: 947px)');
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [open, setOpen] = React.useState(false);
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -181,13 +97,6 @@ export default function Navbar() {
     setAnchorEl(null);
   };
 
-  const handleModalOpen = () => {
-    setOpen(true);
-  };
-
-  const handleModalClose = () => {
-    setOpen(false);
-  };
   return (
     <div className={classes.root}>
       <AppBar className={classes.appbar} position="static">
@@ -199,92 +108,7 @@ export default function Navbar() {
               src={Logo}
             />
           </Link>
-          <Link className={styles.link} to="/tehran">
-            <Button className={classes.cityButton} onClick={handleModalOpen}>
-              <LocationOnIcon /> تهران
-            </Button>
-          </Link>
-          <Modal
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            className={classes.modal}
-            open={open}
-            onClose={handleModalClose}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-              timeout: 500,
-            }}
-          >
-            <Fade in={open}>
-              <div className={classes.paper}>
-                <h3
-                  style={{ fontFamily: 'Vazir', margin: '0px 5px 20px 5px' }}
-                  id="transition-modal-title"
-                >
-                  انتخاب شهر
-                </h3>
-                <TextField
-                  id="outlined-full-width"
-                  style={{ margin: 0 }}
-                  placeholder="جستجوی سریع نام شهر..."
-                  fullWidth
-                  margin="normal"
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  InputProps={{
-                    classes: {
-                      input: classes.resize,
-                    },
-                  }}
-                  variant="outlined"
-                />
-                <p
-                  style={{ fontFamily: 'Vazir', margin: '30px 10px 10px' }}
-                  id="transition-modal-description"
-                >
-                  شهر های پر بازدید
-                </p>
-                <Box>
-                  {/* @ts-ignore */}
-                  {topCities.map((city) => (
-                    <Link style={{ textDecoration: 'none' }} to={`/${city[1]}`}>
-                      {location.pathname === `/${city[1]}` ? (
-                        <Button className={classes.activeCityBtn}>
-                          {city[0]}
-                        </Button>
-                      ) : (
-                        <Button variant="outlined" className={classes.cityBtn}>
-                          {city[0]}
-                        </Button>
-                      )}
-                    </Link>
-                  ))}
-                </Box>
-                <p
-                  style={{ fontFamily: 'Vazir', margin: '30px 10px 10px' }}
-                  id="transition-modal-description"
-                >
-                  همه شهر ها
-                </p>
-                {/* @ts-ignore */}
-                {preLoad.city.compressedData.map((city) => (
-                  <Link style={{ textDecoration: 'none' }} to={`/${city[2]}`}>
-                    {location.pathname === `/${city[2]}` ? (
-                      <Button className={classes.activeCityBtn}>
-                        {city[1]}
-                      </Button>
-                    ) : (
-                      <Button variant="outlined" className={classes.cityBtn}>
-                        {city[1]}
-                      </Button>
-                    )}
-                  </Link>
-                ))}
-              </div>
-            </Fade>
-          </Modal>
+          <CitiesModal />
         </Box>
         <Toolbar className={classes.toolbar}>
           {desktop && (
